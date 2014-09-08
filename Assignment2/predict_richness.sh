86026ffe52576750819918d875f91eb64f62ca36
Carol Rowe <carol.rowe666@gmail.com>
2014-09-07 21:14:45 -0400

    Revert "Let's hope I retrieve all the files I wanted."
    This reverts commit 5ed4036778120ae47ed7f8648af5b69c36b1ddc3.

--- Assignment2/predict_richness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Assignment2 concatenate, sort with unique only, and run through python code.
-
-cat areas*.txt | python rich_pred.py | sort -t',' -nk2 -u > predicted_diversities.txt
-
