1f2e4f37a3a9f8e17155c6af63799d67cdec01b0
Carol Rowe <carol.rowe666@gmail.com>
2014-11-02 19:39:09 -0500

    Adding Assignment8 quest1-4.
    Need to modify def lay_eggs to extend the list. (In other words, I want to extend the list to the current list.) Can not figure that out yet.

--- Assignment8/Assignment8.py
+++ Assignment8/Assignment8.py
@@ -1,4 +1,9 @@
 #Assignment8
+import random
+import pandas as pd
+
+
+
 class Platypus(object):
     def __init__(self, name, eggs_per_season):
         self.name = name
@@ -17,12 +22,49 @@ class Platypus(object):
     def lay_eggs(self, more_eggs):
         #total_eggs = self.total_fecundity()
         #return self.eggs_per_season + [total_eggs] not what you were asking
+        #self.eggs_per_season = self.eggs_per_season.extend(more_eggs)
         self.eggs_per_season = self.eggs_per_season + [more_eggs]
         return self.eggs_per_season
-    
+
+#data simulation
+data = [['Curly', [3]], ['Larry', [2]], ['Moe', [0]]]
+count = 0
+for item in data:
+    #creating a random number of eggs (0 to 3) for 9 times
+    eggs=[random.randint(0,3) for p in range(0,9)]
+    #add the 9 new egg values to the list for each platypus
+    data[count][1].extend(eggs)
+    count = count + 1
+    name = Platypus(item[0], item[1])
+    #print name.lay_eggs(eggs)
+    print "{} laid a total of {} in {} successful breeding seasons.".format(item[0],name.total_fecundity(), name.breeding_seasons())
+print data
+
+
 perry = Platypus("perry", [3, 2, 4, 1, 2])
 print perry.total_fecundity()
 print perry.breeding_seasons()
-#print perry.lay_eggs(perry.total_fecundity()) Oops, this isn't what you were asking!
-print perry.lay_eggs(2)
+#print perry.lay_eggs(perry.total_fecundity()) #Oops, this isn't what you were asking!
+les_oefs = (2)
+print perry.lay_eggs(les_oefs)
+#print perry.eggs_per_season
 print perry.total_fecundity()
+
+platy_data = [Platypus("perry", [3,2,4,1,2]),
+Platypus("quacker", [100,1,3,1,2]),
+Platypus("fishface", [0,1,3,1,2,1]),
+Platypus("duckhead", [3,1,3,6,3]),
+Platypus("waddles", [3,1,2,0,8,3]),
+Platypus("professor quackington", [2,1,4,5,7]),
+Platypus("bartholomew beavertail", [0,1,3,1,0,0,2]),
+Platypus("syd", [3,1,3,1,3,5,5,2,1,3]),
+Platypus("ovide", [2,0,10,0,0,0,0,0,0]),
+Platypus("hexley", [3,1,2,3,1,0,0,1,1]),
+Platypus("supafly", [19,1,2,1,0,0,0,1])]
+"""
+for platy in platy_data:
+    if float(platy.total_fecundity())/platy.breeding_seasons() > 3:
+        print platy.name
+"""    
+names = [platy.name for platy in platy_data if float(platy.total_fecundity())/platy.breeding_seasons() > 3]  
+print '\n'.join(names)
